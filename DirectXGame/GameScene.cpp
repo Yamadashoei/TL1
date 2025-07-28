6399b0deff161e336cf698f15f67da1ed2ec1ddc
Yamadashoei <k023g0053@g.neec.ac.jp>
2025-07-28 16:06:57 +0900

    描画されない。実行は通る

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -4,11 +4,9 @@
 #include "json.hpp"
 #include <cassert>
 #include <fstream>
-#include <string>
 #include <vector>
 
 using namespace KamataEngine;
-using json = nlohmann::json;
 
 GameScene::~GameScene() {}
 
@@ -16,39 +14,12 @@ void GameScene::Initialize() {
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
-
-	// カメラの初期化
 	camera_.Initialize();
 
-	// オブジェクト1個分のデータ
-	struct ObjectData {
-		std::string type;
-		std::string name;
-
-		// transform
-		struct Transform {
-			Vector3 translation;
-			Vector3 rotation;
-			Vector3 scaling;
-		};
-		Transform transform;
-		std::string file_name;
-	};
-
-	//
-	struct LevelData {
-		// name
-		std::string name;
-		// objects
-		std::vector<ObjectData> objects;
-	};
-
-	// JSONファイルを読み込む
+	// JSON読み込み
 	const std::string fullpath = std::string("Resources/levels/") + "scene.json";
-
 	// JSONファイルを開く
 	std::ifstream file;
-
 	// ファイルを開く
 	file.open(fullpath);
 	// ファイルオープン失敗のチェック
@@ -57,81 +28,102 @@ void GameScene::Initialize() {
 	}
 
 	nlohmann::json deserialized;
-
 	// ファイルからJSONを読み込む
 	file >> deserialized;
-
 	// 正しいレベルデータファイルかチェック
 	assert(deserialized.is_object());
 	assert(deserialized.contains("name"));
 	assert(deserialized["name"].is_string());
 
-	// ------------------------------------------------------------
-	// レベルデータを構造体に格納していく
-	// ------------------------------------------------------------
-	LevelData* levelData = new LevelData();
-
-	// "name" を文字列として取得
+	// レベルデータを構造体に格納
+	levelData = new LevelData();
+	// nameを文字列として認識
 	levelData->name = deserialized["name"].get<std::string>();
-	assert(levelData->name == "scene"); // それは "scene" か?
+	// 正しいレベルデータファイルかチェック
+	assert(levelData->name == "scene");
 
-	// "objects" の全オブジェクトを走査
 	for (nlohmann::json& object : deserialized["objects"]) {
-
-		// オブジェクト 1 つ分の妥当性のチェック
-		assert(object.contains("type")); // "type" が含まれているか
-
+		assert(object.contains("type"));
 		if (object["type"].get<std::string>() == "MESH") {
 
-			// 1 個分の要素の準備
+			// 1個分の要素の準備
 			levelData->objects.emplace_back(ObjectData{});
-			ObjectData& objectData = levelData->objects.back(); // 追加要素の参照を用意して可読性も良くなる
-
-			objectData.type = object["type"].get<std::string>(); // "type"
-			objectData.name = object["name"].get<std::string>(); // "name"
-
-			// トランスフォームのパラメータ読み込み
+			ObjectData& objectData = levelData->objects.back();
+			objectData.type = object["type"].get<std::string>();
+			objectData.name = object["name"].get<std::string>();
+			// transformのパラメーター読み込み
 			nlohmann::json& transform = object["transform"];
-			// 平行移動 "translation"
 			objectData.transform.translation.x = (float)transform["translation"][0];
 			objectData.transform.translation.y = (float)transform["translation"][1];
 			objectData.transform.translation.z = (float)transform["translation"][2];
-			// 回転 "rotation"
+
 			objectData.transform.rotation.x = -(float)transform["rotation"][0];
-			objectData.transform.rotation.y = (float)transform["rotation"][2];
+			objectData.transform.rotation.y = -(float)transform["rotation"][2];
 			objectData.transform.rotation.z = (float)transform["rotation"][1];
-			// 拡大縮小 "scaling"
+
 			objectData.transform.scaling.x = (float)transform["scaling"][0];
 			objectData.transform.scaling.y = (float)transform["scaling"][2];
 			objectData.transform.scaling.z = (float)transform["scaling"][1];
 
-			// "file_name"
 			if (object.contains("file_name")) {
 				objectData.file_name = object["file_name"].get<std::string>();
 			}
 		}
 	}
+
+	for (auto& objectData : levelData->objects) {
+		Model* model = nullptr;
+		auto it = models.find(objectData.file_name);
+		if (it == models.end()) {
+			model = Model::CreateFromOBJ(objectData.file_name);
+			models[objectData.file_name] = model;
+		}
+	}
+
+	for (auto& objectData : levelData->objects) {
+		WorldTransform* newObject = new WorldTransform;
+		newObject->translation_ = objectData.transform.translation;
+		newObject->rotation_ = objectData.transform.rotation;
+		newObject->scale_ = objectData.transform.scaling;
+		newObject->Initialize();
+		objects.push_back(newObject);
+	}
+
 }
 
-void GameScene::Update() {}
+void GameScene::Update() {
+	for (WorldTransform* object : objects) {
+		object->TransferMatrix();
+	}
+}
 
 void GameScene::Draw() {
 	ID3D12GraphicsCommandList* commandList = dxCommon_->GetCommandList();
 
-#pragma region 背景スプライト描画
+	// 背景
 	Sprite::PreDraw(commandList);
 	Sprite::PostDraw();
 	dxCommon_->ClearDepthBuffer();
-#pragma endregion
 
-#pragma region 3Dオブジェクト描画
+	// 3Dオブジェクト
 	Model::PreDraw(commandList);
 
-	Model::PostDraw();
-#pragma endregion
+	int i = 0;
+	for (auto& objectData : levelData->objects) {
+		Model* model = nullptr;
+		auto it = models.find(objectData.file_name);
+		if (it != models.end()) {
+			model = it->second;
+		}
+		if (model) {
+			model->Draw(*objects[i], camera_);
+		}
+		i++;
+	}
 
-#pragma region 前景スプライト描画
+	Model::PostDraw();
+
+	// 前景
 	Sprite::PreDraw(commandList);
 	Sprite::PostDraw();
-#pragma endregion
 }
--- DirectXGame/GameScene.h
+++ DirectXGame/GameScene.h
@@ -1,11 +1,33 @@
 #pragma once
 #include "KamataEngine.h"
+#include <2d/Sprite.h>
 #include <3d/Camera.h>
 #include <3d/Model.h>
 #include <3d/WorldTransform.h>
 
+#include <map>
+#include <string>
+#include <vector>
 
 class GameScene {
+
+	struct ObjectData {
+		std::string type;
+		std::string name;
+		struct Transform {
+			KamataEngine::Vector3 translation;
+			KamataEngine::Vector3 rotation;
+			KamataEngine::Vector3 scaling;
+		};
+		Transform transform;
+		std::string file_name;
+	};
+
+	struct LevelData {
+		std::string name;
+		std::vector<ObjectData> objects;
+	};
+
 public:
 	~GameScene();
 	void Initialize();
@@ -16,9 +38,10 @@ private:
 	KamataEngine::DirectXCommon* dxCommon_ = nullptr;
 	KamataEngine::Input* input_ = nullptr;
 	KamataEngine::Audio* audio_ = nullptr;
-
-	// カメラ
 	KamataEngine::Camera camera_;
 
-	
+	LevelData* levelData = nullptr; 
+	std::map<std::string, KamataEngine::Model*> models;
+
+	std::vector<KamataEngine::WorldTransform*> objects;
 };
